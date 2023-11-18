2649e97bc4f79e6acaa0f34557234db3fd6caf4e
Gil Zanabria <gzanabriaz28@gmail.com>
2023-11-18 12:51:43 -0500

    user auth in readme

--- app/src/main/java/com/example/lab7_20200334_iot/dtos/Usuario.java
+++ app/src/main/java/com/example/lab7_20200334_iot/dtos/Usuario.java
@@ -8,6 +8,16 @@ public class Usuario implements Serializable {
     private String apellido;
     private String email;
 
+    public String getNombreSalon() {
+        return nombreSalon;
+    }
+
+    public void setNombreSalon(String nombreSalon) {
+        this.nombreSalon = nombreSalon;
+    }
+
+    private String nombreSalon;
+
     public String getEmail() {
         return email;
     }
